15c38ac70775a44cd79054bd2c1eaa9d67bc3e09
Ivan Sekulic <ivansekulic0@gmail.com>
2017-01-19 10:50:26 +0100

    dodana referenca u doc

--- src/eval.sh
+++ src/eval.sh
@@ -36,21 +36,21 @@ fi
 echo "Our score after 1. iteration:"
 cat out.report | grep AvgId | tr -s ' ' | cut -d' ' -f2 | head -1
 
-echo "#### Running 2. iteration"
+#echo "#### Running 2. iteration"
 
 # TODO: maybe run mappings again? yes
 #echo "Generating mappings from reads (.sam)"
 #../graphmap/bin/Linux-x64/graphmap align -r ../data/"$NAME"_final -d ../data/"$NAME"_reads.fastq -o ../data/"$NAME"_mappings2.sam
 
-echo "#2 Running readings for $NAME"
-./reading ../data/"$NAME"_final ../data/"$NAME"_mappings.sam ../data/"$NAME"_nas.format2
+#echo "#2 Running readings for $NAME"
+#./reading ../data/"$NAME"_final ../data/"$NAME"_mappings.sam ../data/"$NAME"_nas.format2
 
-echo "#2 Running sparc."
-./sparc < ../data/"$NAME"_nas.format2 > ../data/"$NAME"_final2
+#echo "#2 Running sparc."
+#./sparc < ../data/"$NAME"_nas.format2 > ../data/"$NAME"_final2
 
-echo "#2 Running dnadiff"
-dnadiff ../data/"$NAME"_reference.fasta ../data/"$NAME"_final2
+#echo "#2 Running dnadiff"
+#dnadiff ../data/"$NAME"_reference.fasta ../data/"$NAME"_final2
 
-echo "Our score after 2. iteration:"
-cat out.report | grep AvgId | tr -s ' ' | cut -d' ' -f2 | head -1
+#echo "Our score after 2. iteration:"
+#cat out.report | grep AvgId | tr -s ' ' | cut -d' ' -f2 | head -1
 
